5f0679273d1e62bc23ead35ed26325817e3fc7ee
syedamir5560 <syedamir5560@gmail.com>
2024-02-24 14:31:41 +0530

    khk

--- src/AddBlog.js
+++ src/AddBlog.js
@@ -1,6 +1,6 @@
 import { MDBValidation, MDBInput, mdbOn, MDBBtn } from 'mdb-react-ui-kit'
 import React, { useState } from 'react'
-import { Toast } from "react-toastify"
+import { Toast, toast } from "react-toastify"
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
@@ -29,30 +29,52 @@ function AddBlog() {
 
   }
 
-  const onInputChange = () => {
+  const onInputChange = (e) => {
+
+    setFormValue({
+      ...formValue,[e.target.name]:e.target.value
+    })
 
   }
 
-  const onCategoryChange = () => {
+  const onCategoryChange = (e) => {
+    setFormValue({
+      ...formValue , category:e.target.value
+    })
 
   }
 
   const onUploadImage = (file) => {
-    console.log(file,"file")
+    // console.log(file,"file")
 
+    const formData = new FormData()
+    formData.append("file", file)
+    // formData.append('Clo')
+    formData.append('upload_preset', 'i8thgtdk')
+    axios.post('http://api.cloudinary.com/v1_1/do4lomwfi/image/upload', formData)
+      .then((res) => {
+        // console.log(res)
+        toast.info('upload image succesfully')
+        setFormValue({
+          ...formValue , imgUrl:res.data.url
+        })
+      }).catch((error) => {
+        // console.log(error)
+        toast.info("Something went wrong")
+      })
   }
 
   return (
-    <div style={{border:'2px solid brown'}}>
+    <div style={{ border: '2px solid brown' }}>
       <MDBValidation
         onSubmit={handleSubmit}
         className='row g-2'
-        style={{ marginTop: "100px" ,padding:'100px'}}
+        style={{ marginTop: "100px", padding: '100px' }}
         noValidate
 
       >
         <p className='fs-3 fw-bold'
-          style={{ margin: 'auto', maxWidth: '400px', alignContent: 'center',  }}>Add Blog</p>
+          style={{ margin: 'auto', maxWidth: '400px', alignContent: 'center', }}>Add Blog</p>
 
         <MDBInput
           value={title || ""}
@@ -64,7 +86,7 @@ function AddBlog() {
           required
           validation='please provoide title'
           invalid
-        
+
         />
 
         <MDBInput
@@ -78,11 +100,10 @@ function AddBlog() {
           rows={4}
           required
           validation='please provoide descripton'
-          invalid
+          invalid='true'
         />
-        
-        <MDBInput
 
+        <MDBInput
           name='category'
           onChange={(e) => onUploadImage(e.target.files[0])}
           label='Category'
@@ -94,27 +115,27 @@ function AddBlog() {
         />
         <br />
 
-       
+
         <br />
 
         <select className='categoryDropdown' onChange={onCategoryChange} value={category}>
           <option>Select Category</option>
           {
-            option.map((option , index)=>(
-                <option value={option || ""} key={index}>
-                    {option}
-                </option>  
+            option.map((option, index) => (
+              <option value={option || ""} key={index}>
+                {option}
+              </option>
             ))
           }
         </select>
-        <br/>
-        <br/>
+        <br />
+        <br />
 
-        <MDBBtn type='submit' style={{margin:'auto',width:"40%",marginTop:'20px'}}>Add</MDBBtn>
+        <MDBBtn type='submit' style={{ margin: 'auto', width: "40%", marginTop: '20px' }}>Add</MDBBtn>
 
-        <br/>
+        <br />
 
-        <MDBBtn color='danger' style={{margin:'auto',width:"40%",marginTop:'20px'}} >Go Back  </MDBBtn> 
+        <MDBBtn color='danger' style={{ margin: 'auto', width: "40%", marginTop: '20px' }} >Go Back  </MDBBtn>
 
       </MDBValidation>
 
